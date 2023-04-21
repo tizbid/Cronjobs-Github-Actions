6e5d9a7f16786b2bb069690d65061726110dba40
tizbid <tizbid@gmail.com>
2023-04-21 02:54:11 +0200

    modified file structure

---
+++ github-action-template/main.py
@@ -0,0 +1,65 @@
+import logging
+import logging.handlers
+import os
+import requests
+import tweepy
+
+# Enter your Twitter API keys and tokens here
+consumer_key = "FP6s8kozqDOUSdNnlhwvZdks6"
+consumer_secret = "EvuE4uY3pGpwyW2IxAIFTP0XaYn7mTGPNOVH6M32tkItKHFdCc"
+access_token = "2768144092-dFxSnQ77jRPBBRYLMesK9iQSHMRJm9Ro4U5ODrh"
+access_token_secret = "3KE8jIxMjJZvKwjaQd3k6qa6egdWjzIUKir1tXtCqBY9v"
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "status.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
+
+try:
+    consumer_key = os.environ["CONSUMER_KEY"]
+    consumer_secret = os.environ["CONSUMER_SECRET"]
+    access_token= os.environ["ACCESS_TOKEN"]
+    access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
+except KeyError:
+    logger.info("Authentication failed!")
+  
+
+
+if __name__ == "__main__":
+    
+    logger.info("Authentication for Twitter API successful!")
+    
+    # Authenticate with Twitter API
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    
+    # Create API object
+    api = tweepy.API(auth)
+
+    # Get the current trending topics for the specified location (here, global)
+    trends = api.get_place_trends(id=23424829)
+
+
+    #Find the maximum tweet volume
+    max_tweet_volume = max(trend["tweet_volume"] or 0 for trend in trends[0]["trends"])
+
+    # Loop through the trends and print the name and tweet volume
+    for trend in trends[0]["trends"]:
+        if trend["tweet_volume"] == max_tweet_volume:
+            most_trending_topic = trend["name"]
+            
+    #Log in the most trending topic
+    logger.info(f'Todays trending Tweeter Topic in Germany is: {most_trending_topic} with  {max_tweet_volume} tweets')
+    
+    
+    
+    
+    
