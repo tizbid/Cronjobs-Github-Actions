69bdd07cd0a73975a92ccfe44d07636dbd5076a0
tizbid <tizbid@gmail.com>
2023-04-23 01:55:17 +0200

    Added comments to main.py

--- main.py
+++ main.py
@@ -4,27 +4,33 @@ import os
 import tweepy
 
 # Set up logging
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
+# Define a RotatingFileHandler to handle logging output
 logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
+    "status.log",  # File name
+    maxBytes=1024 * 1024,  # Maximum size in bytes before rollover
+    backupCount=1,  # Number of backup files to keep
+    encoding="utf8",  # File encoding
 )
+
+# Set the log message format
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+# Attach the log message format to the logger
 logger_file_handler.setFormatter(formatter)
 logger.addHandler(logger_file_handler)
 
+# Attempt to retrieve the required authentication tokens from the environment variables
 try:
     CONSUMER_KEY = os.environ["CONSUMER_KEY"]
     CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
     ACCESS_TOKEN= os.environ["ACCESS_TOKEN"]
     ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
 except KeyError:
+    # Log an error message if any of the required environment variables are missing
     logger.info("Authentication failed!")
-  
 
 
 if __name__ == "__main__":
